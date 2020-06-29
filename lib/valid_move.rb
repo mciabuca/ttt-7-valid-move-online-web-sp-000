d925f2c24c42f9028b92d918940a8766f1236606
Michael Ciabuca <ciabuca@gmail.com>
2020-06-29 02:33:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if board.any? && position_taken?(board, index) && index.between?(0, 8)
     return false
-  elsif boardy.any? == false && position_taken?(board, index) == false && index.between?(0 ,8)
+  elsif board.any? == false && position_taken?(board, index) == false && index.between?(0 ,8)
     return true
  end
 end
