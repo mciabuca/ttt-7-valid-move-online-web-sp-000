c43360f003feab149f0ea27199eec7859607830a
Michael Ciabuca <ciabuca@gmail.com>
2020-06-29 05:11:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if board.any? && position_taken?(board, index)
-    return false
-  elsif index.between?(0, 8) && board.any? == false
-    return true
-  elsif !board.any? && !position_taken?(board, index)
-    return false
- end
+  
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
